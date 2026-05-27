548acdbbc77889cb8efa2def7b8aa8c902b8bab5
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-05-27 16:46:43 +0530

    fix(backend): export wishlist model from schema file

--- backend/db/wishlist.js
+++ backend/db/wishlist.js
@@ -1,8 +1,10 @@
-const mongoose=require("mongoose")
+const mongoose = require("mongoose");
 
-const wishlistSchema= new mongoose.Schema({
-userId:{type: mongoose.Schema.Types.ObjectId, ref:"users"},
-productsId:[String]
-})
+const wishlistSchema = new mongoose.Schema({
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+    productsId: [String]
+});
 
-const Wishlist= mongoose.model("wishlists", wishlistSchema)
+const Wishlist = mongoose.model("wishlists", wishlistSchema);
+
+module.exports = Wishlist;

dee3bdf3d2ae2506c10e277b9f10e75a02049254
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-05-27 17:51:57 +0530

    feat(backend): add MongoDB connection setup using mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,17 @@
 const express=require("express")
 
+const mongoose=require("mongoose")
+
+async function connectDb(){
+    await mongoose.connect('mongodb://localhost:27017',{
+        dbName:'CartNova'
+    })
+    console.log("Connected")
+}
+connectDb().catch((err)=>{
+    console.error(err)
+})
+
 const app=express()
 
 const port=3000

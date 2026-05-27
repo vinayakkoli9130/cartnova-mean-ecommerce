520bac71a2b4c88b2ddc7ac6d35a53831c34b7c4
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-05-27 16:27:59 +0530

    feat(backend): add order mongoose schema

---
+++ backend/db/order.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    date: Date,
+    items: [],
+    status: Number
+
+});
+
+const Order = mongoose.model("orders", orderSchema);
+
+module.exports = Order;

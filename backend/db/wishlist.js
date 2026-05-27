04d3f346bf367f80e1cf29cd986eceb8dcbcaf5f
Vinayak Koli <vinayakkoli9130@gmail.com>
2026-05-27 16:38:30 +0530

    feat(backend): add wishlist mongoose schema

---
+++ backend/db/wishlist.js
@@ -0,0 +1,8 @@
+const mongoose=require("mongoose")
+
+const wishlistSchema= new mongoose.Schema({
+userId:{type: mongoose.Schema.Types.ObjectId, ref:"users"},
+productsId:[String]
+})
+
+const Wishlist= mongoose.model("wishlists", wishlistSchema)
